ac45beeca033c13ddf5997636905f64f9c6294a0
alinafekamwendo <48163686+alinafekamwendo@users.noreply.github.com>
2022-01-12 23:40:35 +0200

    Update Breeds.js

--- server/src/models/Breeds.js
+++ server/src/models/Breeds.js
@@ -1,5 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
     const Breeds = sequelize.define("Breeds", {
+
+      breedId:{
+        type: DataTypes.BIGINT,
+         autoIncrement: true, 
+         allowNull: false,
+          primaryKey: true ,
+
+      },
       breedName: {
         type: DataTypes.STRING,
         allowNull: false,
